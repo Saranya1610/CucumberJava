7b2b18f4e8fc300c79759199bf66a199ad4d0080
Sara <sara161091@gmail.com>
2023-02-22 15:52:23 +0000

    commiting from intellj

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Page pg = new Page();
+        pg.sum();
+        pg.sub();
+        pg.mul();
+        pg.div();
+    }
+}
---
+++ src/main/java/org/example/Page.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Page {
+    int a = 30;
+    int b = 10;
+    void sum(){
+        int c = a+b;
+        System.out.println(c);
+    }
+    void sub(){
+        int c = a-b;
+        System.out.println(c);
+
+    }
+    void mul(){
+        int c = a*b;
+        System.out.println(c);
+
+    }
+    void div(){
+        int c = a/b;
+        System.out.println(c);
+
+    }
+}
